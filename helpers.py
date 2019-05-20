50191711fa21a9b075f2810bb9a8ce2e8fdc67fd
Steve Porter <sjporter399@gmail.com>
2019-05-19 22:21:37 -0600

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,10 +1,21 @@
-
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 def alphabet_position(letter):
-    #Add your code here
-    return
 
-
+    let_pos = alphabet.index(letter.capitalize())
+    
+    return let_pos
+    
 def rotate_character(char, rot):
-    #Add your code here
-    return
+
+    if char.isalpha() == False:
+        return char
+
+    rot_pos = (alphabet_position(char) + rot) % 26
+    rot_letter = alphabet[rot_pos]
+    
+    if char.islower():
+        return rot_letter.lower()
+    else:
+        return rot_letter  
